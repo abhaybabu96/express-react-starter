adf3e054885bcf9c5a8d4dda2120f52e3cb3bf94
Abhay Babu <your_email@example.com>
2025-11-13 18:26:23 +0530

    Shopify Porduct API

---
+++ server/index.js
@@ -0,0 +1,33 @@
+// const express = require('express');
+// const app = express();
+
+// app.get('/api/test', (req, res) => {
+//   res.send('Server is running!');
+// });
+
+// module.exports = app;
+
+
+// server/index.js
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+
+// Shopify products API
+router.get('/api/products', async (req, res) => {
+  try {
+    const {env :{SHOPIFY_STORE, SHOPIFY_ADMIN_TOKEN}} = process
+    const response = await axios.get(SHOPIFY_STORE, {
+      headers: {
+        'X-Shopify-Access-Token': SHOPIFY_ADMIN_TOKEN,
+        'Content-Type': 'application/json',
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching products:', error.message);
+    res.status(500).json({ error: 'Failed to fetch products' });
+  }
+});
+
+module.exports = router;
