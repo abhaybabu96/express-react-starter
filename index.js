7361c9e09b3b5e8aa04da3f351c017659c39a862
Abhay Babu <your_email@example.com>
2025-11-13 18:33:14 +0530

    Chagnes in main index.js file

--- index.js
+++ index.js
@@ -3,43 +3,51 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
+// Load environment variables
+require('dotenv').config();
+
 // Set EJS as the template engine
 app.set('view engine', 'ejs');
 
 // Home route
 app.use(express.static('public'));
 
-// dynamic home
+//dynamic home
 app.get('/', (req, res) => {
   const user = "Abhay Babu";
   res.render('index', { username: user });
 });
 
-// About Route
-app.get('/about', (req, res) => {
-  const info = "About page after installing the nodemon!";
-  res.render('about', { infoText: info });
-});
-
-// Contact Route
-app.get('/contact', (req, res) => {
-  const info = "This is using the contact page using ejs!";
-  res.render('about', { infoText: info });
-});
-
-// Contact Route
-app.get('/privacy', (req, res) => {
-  const info = "This Privacy page using ejs!";
-  res.render('about', { infoText: info });
-});
-
-app.get('/api', (req, res) => {
-  const info = "This Privacy page using ejs!";
-  res.render('about', { infoText: info });
-});
-
-
+// Import the backend routes
+const shopifyRoutes = require('./server/index');
+app.use('/', shopifyRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+// // About Route
+// app.get('/about', (req, res) => {
+//   const info = "About page after installing the nodemon!";
+//   res.render('about', { infoText: info });
+// });
+
+// // Contact Route
+// app.get('/contact', (req, res) => {
+//   const info = "This is using the contact page using ejs!";
+//   res.render('about', { infoText: info });
+// });
+
+// // Contact Route
+// app.get('/privacy', (req, res) => {
+//   const info = "This Privacy page using ejs!";
+//   res.render('about', { infoText: info });
+// });
+
+// app.get('/api', (req, res) => {
+//   const info = "This Privacy page using ejs!";
+//   res.render('about', { infoText: info });
+// });
+
+
+
